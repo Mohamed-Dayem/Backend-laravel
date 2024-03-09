a1c833a89c436e3cd7344a6df9fb5ab7dc6c6cf5
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:33:01 +0300

    comment

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -4,16 +4,29 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
+/**
+ * Class TrimStrings
+ * 
+ * This middleware class extends the base TrimStrings middleware provided by Laravel.
+ * Its primary function is to trim excess white space from input data.
+ * However, it has a list of exceptions where trimming is not applied.
+ *
+ * @package App\Http\Middleware
+ */
 class TrimStrings extends Middleware
 {
     /**
      * The names of the attributes that should not be trimmed.
      *
+     * An array containing the names of the attributes that should not be trimmed.
+     * These attributes are typically related to user authentication and sensitive data.
+     *
      * @var array<int, string>
      */
     protected $except = [
-        'current_password',
-        'password',
-        'password_confirmation',
+        'current_password', // current user's password for authentication
+        'password', // new user's password during registration or password change
+        'password_confirmation' // confirmation of the new user's password
     ];
 }
+
