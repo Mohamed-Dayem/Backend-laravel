dc03f21e1247b08193f5984d8d314e7d251ce68e
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:33:04 +0300

    comment

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -5,17 +5,29 @@ namespace Tests;
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
 
+/**
+ * Trait for creating the application instance.
+ */
 trait CreatesApplication
 {
     /**
-     * Creates the application.
+     * Creates and configures a new application instance.
+     *
+     * This method should be called before any tests are executed to ensure
+     * the application is in a consistent state.
+     *
+     * @return Application The application instance.
      */
     public function createApplication(): Application
     {
+        // Include the bootstrap/app.php file which sets up the application.
         $app = require __DIR__.'/../bootstrap/app.php';
 
+        // Bootstrap the application's service providers.
         $app->make(Kernel::class)->bootstrap();
 
+        // Return the application instance.
         return $app;
     }
 }
+
