a98f706bb17367c7c09218ebfac65b2268ba8697
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:16:10 +0300

    improve

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-import './bootstrap';
+// Import specific components or functions from your Laravel application's bootstrap.js file
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+
+// Import your application's CSS file
+import '../css/app.css';
+
+// Configure Inertia.js
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => import(`./Pages/${name}.vue`),
+  setup({ el, App, props }) {
+    const app = createApp({ render: () => h(App, props) });
+
+    // Register global components
+    app.component('example-component', require('./Components/ExampleComponent.vue').default);
+
+    app.mount(el);
+  },
+});
+
+// Initialize Inertia Progress
+InertiaProgress.init({ color: '#4B5BD5' });
