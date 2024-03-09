04df9c1eb5677ed9ecbb549766c505dcec9d1c4d
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:33:01 +0300

    comment

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -11,14 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Create a new table named 'personal_access_tokens'
         Schema::create('personal_access_tokens', function (Blueprint $table) {
+            // Add an auto-incrementing primary key column named 'id'
             $table->id();
+            // Add a morphs relationship with 'tokenable' model
             $table->morphs('tokenable');
+            // Add a string column named 'name'
             $table->string('name');
+            // Add a unique string column named 'token' with length of 64 characters
             $table->string('token', 64)->unique();
+            // Add a text column named 'abilities' that can store null values
             $table->text('abilities')->nullable();
+            // Add a timestamp column named 'last_used_at' that can store null values
             $table->timestamp('last_used_at')->nullable();
+            // Add a timestamp column named 'expires_at' that can store null values
             $table->timestamp('expires_at')->nullable();
+            // Add timestamps columns 'created_at' and 'updated_at'
             $table->timestamps();
         });
     }
@@ -27,7 +36,4 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
-    {
-        Schema::dropIfExists('personal_access_tokens');
-    }
-};
+
