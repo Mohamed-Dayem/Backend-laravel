432c7a00227b1619f28afcb04744a3742e5e44a8
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:32:59 +0300

    comment

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,20 @@
 <?php
 
+// Import necessary namespaces
 namespace App\Http\Controllers;
 
+// BaseController class is extended to provide a foundation for all Controllers
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+// Import BaseController class
 use Illuminate\Routing\Controller as BaseController;
 
+// Controller class extends BaseController and includes two traits for authorization and validation
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    // Use AuthorizesRequests trait for authorization checks
+    use AuthorizesRequests,
+        // Use ValidatesRequests trait for request validation
+        ValidatesRequests;
 }
+
