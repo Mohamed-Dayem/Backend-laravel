9050f4bca9621db2cbae3a79b132a8812289f6af
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:16:03 +0300

    improve

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -9,9 +9,5 @@ class EncryptCookies extends Middleware
     /**
      * The names of the cookies that should not be encrypted.
      *
-     * @var array<int, string>
-     */
-    protected $except = [
-        //
-    ];
-}
+     * This array should include any cookies that should not be encrypted, such as
+    
