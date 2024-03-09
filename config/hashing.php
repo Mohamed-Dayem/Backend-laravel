863cfe5ef794a472af66a38c5a479b4a80d76828
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:32:59 +0300

    comment

--- config/hashing.php
+++ config/hashing.php
@@ -1,5 +1,7 @@
 <?php
 
+// This is a configuration file for password hashing in an application.
+
 return [
 
     /*
@@ -7,48 +9,14 @@ return [
     | Default Hash Driver
     |--------------------------------------------------------------------------
     |
-    | This option controls the default hash driver that will be used to hash
-    | passwords for your application. By default, the bcrypt algorithm is
-    | used; however, you remain free to modify this option if you wish.
-    |
-    | Supported: "bcrypt", "argon", "argon2id"
+    | This option specifies the default hash driver used to hash passwords for
+    | the application. The supported drivers are: "bcrypt", "argon", and
+    | "argon2id".
     |
     */
-
     'driver' => 'bcrypt',
 
     /*
     |--------------------------------------------------------------------------
     | Bcrypt Options
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the configuration options that should be used when
-    | passwords are hashed using the Bcrypt algorithm. This will allow you
-    | to control the amount of time it takes to hash the given password.
-    |
-    */
-
-    'bcrypt' => [
-        'rounds' => env('BCRYPT_ROUNDS', 12),
-        'verify' => true,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Argon Options
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the configuration options that should be used when
-    | passwords are hashed using the Argon algorithm. These will allow you
-    | to control the amount of time it takes to hash the given password.
-    |
-    */
-
-    'argon' => [
-        'memory' => 65536,
-        'threads' => 1,
-        'time' => 4,
-        'verify' => true,
-    ],
-
-];
+    |------------------------------------------------
