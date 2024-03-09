0b9bfbf38b2302f1fe3cb41786c2bbee67a5985e
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:32:59 +0300

    comment

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -4,14 +4,25 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
 
+/**
+ * This class, PreventRequestsDuringMaintenance, extends the Middleware
+ * class provided by Laravel's foundation. It's responsible for preventing
+ * requests from reaching the application while maintenance mode is enabled.
+ */
 class PreventRequestsDuringMaintenance extends Middleware
 {
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
      *
+     * An array of URI patterns that will be allowed access even when
+     * the application is in maintenance mode. This can be useful for
+     * allowing specific pages, such as a maintenance mode landing page,
+     * to still be accessible.
+     *
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // Add URI patterns to this array to allow access during maintenance mode.
     ];
 }
+
