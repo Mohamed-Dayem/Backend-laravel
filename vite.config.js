2392f6a2fe98da392bf6ecb58990a75313ff013c
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:16:09 +0300

    improve

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravelPlugin from 'laravel-vite-plugin';
+
+const inputFiles = ['resources/css/app.css', 'resources/js/app.js'];
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravelPlugin({
+      input: inputFiles,
+      refresh: true,
+    }),
+  ],
 });
