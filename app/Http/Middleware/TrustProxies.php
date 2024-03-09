353f451c304f18f25cc60d43ec85a1831fff0e6c
Mohamed-Dayem <141914187+Mohamed-Dayem@users.noreply.github.com>
2024-03-09 18:16:07 +0300

    improve

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -25,4 +25,29 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  array<int, string>|string|null  $proxies
+     * @return void
+     */
+    public function __construct($proxies = null)
+    {
+        parent::__construct($proxies);
+
+        // Set the headers property to a new instance of \Illuminate\Http\Request
+        // so that we can use the defined constants
+        $this->headers = request()->headerNames();
+        $this->headers = array_merge(
+            $this->headers,
+            [
+                'x-forwarded-for' => 'X_FORWARDED_FOR',
+                'x-forwarded-host' => 'X_FORWARDED_HOST',
+                'x-forwarded-port' => 'X_FORWARDED_PORT',
+                'x-forwarded-proto' => 'X_FORWARDED_PROTO',
+                'x-forwarded-aws-elb' => 'X_FORWARDED_AWS_ELB'
+            ]
+        );
+    }
 }
